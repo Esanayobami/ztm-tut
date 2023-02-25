771716a9ba05984e249975acea91f475dd4593a1
dicey <esanelijah@icloud.com>
2023-02-25 03:51:36 -0800

    filtering data through the input

--- src/App.js
+++ src/App.js
@@ -31,6 +31,25 @@ class App extends Component {
     render() {
       return (
         <div className="App">
+          <input
+           className="search-box"
+           type="search"
+            placeholder="search monsters :/ "
+            onChange={(event) => {
+              // logs the input value to the console
+              console.log(event.target.value);
+              // set the value to a lower case so its not case sensitive
+              const searchString = event.target.value.toLocaleLowerCase();
+              // filters the data from the input value eg if the input value contains "a" and the starting value of the monsters name start with "a" its returns the monster name
+              const filteredStrings = this.state.monsters.filter((monster) => {
+               return monster.name.toLocaleLowerCase().includes(event.target.value)
+              });
+              // assigning the monsters data to the filter code
+              this.setState(() =>{
+                return { monsters : filteredStrings}
+              })
+            }}
+           />
           {this.state.monsters.map(monster => (
         <div key={monster.id}>
           <h1>{monster.name}</h1>
